0c80e611a2de515022571cef0b3b15e3d2413552
Vejay Drepaul <vejayddrepaul@gmail.com>
2022-07-20 23:29:44 -0400

    Multiplication of every second number working

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-import array as arr
-
-from numpy import number 
 
 def main():
-    #Driver code
-    cc = input("Enter your credit card number: ")
-    validate_card(cc)
-
-def validate_card(card_number):
-    numbers = []
-    for x in card_number: 
-        numbers.append(x)
+    card_no = input("Enter the credit card number: ")[::-1]
+    check_card(card_no)
 
 
-    numbers.reverse()
+def check_card(card_number):
+    sum = 0
+    temp = 0
+    product_values = []
 
-    
-    for element in numbers[1::2]:
-        print(element)
-        
+    letters = (
+        # i % n == 0 means this letter should be replaced
+        "0" if i % 2 == 0 else char
 
-main()
+        # iterate index/value pairs
+        for i, char in enumerate(card_number, 1)
+    )
+    card_number = ''.join(letters)
+    print(card_number)
+
+    for x in card_number:
+        for n in str(x):
+            temp = int(n) * 2
+            product_values.append(temp)
+
+    for u in product_values:
+        print(u)
+
+    for y in product_values:
+        if int(y) > 9:
+            for digit in str(y):
+                sum += int(digit)
+        else:
+            sum += int(y)
+
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
